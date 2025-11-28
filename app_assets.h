24f6fb2738046b68397ba0a0fe7a74c16450ce92
studio-a-productions <alexander.siebens@icloud.com>
2025-11-28 11:14:10 +0100

    fixed/added #ifndef

--- app_assets.h
+++ app_assets.h
@@ -1,6 +1,11 @@
+#ifndef APP_ASSETS_H
+#define APP_ASSETS_H
+
 #include "common_libs.h"
 
 typedef struct {
     // This will be the asset attribute pointer/empty, easy?
     int a;
 } app_asset;
+
+#endif
--- app_render.h
+++ app_render.h
@@ -1,5 +1,5 @@
-#ifndef RENDER_H
-#define RENDER_H
+#ifndef APP_RENDER_H
+#define APP_RENDER_H
 #include "common_libs.h"
 
 typedef struct {
